f3953ee36fadc9b296c3c89c3a3dc82e9c5afb51
stephen.easley-walsh <stephen.easley-walsh@studygroup.com>
2021-11-15 14:35:21 +0000

    Update

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-# Ask for a, b and c integer coefficents and calculate the delta
+# Ask for a, b and c integer coefficients and calculate the delta
 print("Solver for ax^2 + bx + c = 0 where a,b,c are integers")
 a = int(input("What is a?"))
 b = int(input("What is b?"))
 c = int(input("What is c?"))
-delta = b ** 2 - 4 * a * c
+delta = b**2 - 4*a*c
 
 # The different cases
-if (a == 0):
-    print("That isn't a quadratic! a can't be zero!")
-elif (delta > 0):
+if a == 0:
+    print("That is not a quadratic! The value of a cannnot be zero!")
+elif delta > 0:
     print("The roots are", (-b - delta ** 0.5) / (2 * a), "and", (-b + delta ** 0.5) / (2 * a))
-elif (delta == 0):
+elif delta == 0:
     print("The root is", (-b) / (2 * a))
-elif (delta < 0):
+elif delta < 0:
     print("There are no real roots")
 else:
     print("Unknown error.")
